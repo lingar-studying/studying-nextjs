637e30dd3cc10e73ebdd2f4e647db55f5d45debc
lingar <yimprogramming@gmail.com>
2025-02-21 00:21:23 +0200

    Adding mock

---
+++ src/pages/bookmark-app/mock-data-bookmarks.js
@@ -0,0 +1,33 @@
+export const mockBookmarks = [
+    {
+        bookName: "Nedarim",
+        sectionNum: 2,
+        currentPage: 21,
+        quote: "Nadar Baherem Ve'Amar...",
+        isActiveLast2Weeks: true
+    },
+    {
+        bookName: "Likutey Moharan Tinyana",
+        sectionNum: 47,
+        currentPage: 27,
+        quote: "Gedola Lomar Torah",
+        isActiveLast2Weeks: true
+    },
+    {
+        bookName: "Kombinatorika IV",
+        sectionNum: 2,
+        currentPage: 47,
+        quote: "Zeruf Shel k Evarim...",
+        isActiveLast2Weeks: false
+    }];
+/*
+bookName – string,
+currentPage – number,
+quote – string// the accurate place that the reader stand.
+SectionNum,
+Comment,
+isActiveLastTwoWeeks.
+
+ */
+
+
