cdd2998c0656387a8f23f09bfe11d7ed87b9f79e
lingar <yimprogramming@gmail.com>
2025-02-20 23:44:56 +0200

    Adding some component

---
+++ src/pages/draft-area/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// export default function Draft (){
+// }
+
+const DraftDefault = () => {
+
+    return (
+        <>
+            <h1>Hi I am draft Default</h1>
+        </>
+    )
+}
+export default DraftDefault;
---
+++ src/pages/draft-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// export default function Draft (){
+// }
+
+const Draft = () => {
+
+    return (
+        <>
+            <h1>Hi I am draft</h1>
+        </>
+    )
+}
+export default Draft;
