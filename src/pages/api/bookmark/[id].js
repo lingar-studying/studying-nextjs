2c7fa9db05ee0f24a79480123a6de72d82e17382
lingar <yimprogramming@gmail.com>
2025-02-25 15:28:46 +0200

    Creating by id webservice

---
+++ src/pages/api/bookmark/[id].js
@@ -0,0 +1,11 @@
+import {bookmarksMockServer} from "../../../server/bookmarks/bookmarks-mock-server";
+
+export default function handler(req, res) {
+    const { id } = req.query;
+    const item = bookmarksMockServer.filter(item=> item.id === +id);
+
+
+    if (req.method === 'GET') {
+        res.status(200).json(item);
+    }
+}
