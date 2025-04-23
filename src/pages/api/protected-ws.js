903e94f2c6ced7f997207f4e1029f6b9c6a0bee4
lingar <yimprogramming@gmail.com>
2025-04-23 18:55:31 +0300

    Example-How debug help to find the problem

--- src/pages/api/protected-ws.js
+++ src/pages/api/protected-ws.js
@@ -1,14 +1,19 @@
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
+import * as logger from "next/dist/build/output/log";
 
 const SECRET = process.env.JWT_SECRET || "some-custom-your-secret-key"; //'mytestkey'; - won't work - the same key should come here....
 
 export default function handler(req, res) {
     try {
+        console.log("hhhh")
         const cookies = cookie.parse(req.headers.cookie || '');
         const token = cookies.token;
+        console.log("token = ", token);
 
         if (!token) {
+            logger.error(err);
+
             return res.status(401).json({ error: 'Missing token' });
         }
 
@@ -17,6 +22,7 @@ export default function handler(req, res) {
 
         res.status(200).json({ message: 'Welcome!', user });
     } catch (err) {
-        res.status(401).json({ error: 'Invalid or expired token' });
+        logger.error(err);
+        res.status(401).json({ error: 'Invalid or expired toksn' });
     }
 }
