19211b00bd2dc8692186fd5e2541463134a2ac33
lingar <yimprogramming@gmail.com>
2025-07-11 02:59:36 +0300

    Installing and Trying- but problem

--- src/pages/pdf-poc.js
+++ src/pages/pdf-poc.js
@@ -1,10 +1,30 @@
-import React  from "react";
+import React, {useEffect} from "react";
+import puppeteer from 'puppeteer';
+
 
 
 
 //POC of PDF generator
 const PdfPoc = () =>{
 
+
+    const generatePdf = async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://news.ycombinator.com', {
+            waitUntil: 'networkidle2',
+        });
+// Saves the PDF to hn.pdf.
+        await page.pdf({
+            path: 'hn.pdf',
+        });
+
+        await browser.close();
+    }
+
+    useEffect(() => {
+        generatePdf();
+    }, []);
     return(
         <h1>PDF POC</h1>
     )
